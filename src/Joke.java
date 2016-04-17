49ef975ea75264aef779a2b770454b3344271ccb
Taylor Miller <trmiller10@gmail.com>
2016-04-17 19:15:55 -0400

    Created Joke.java.

---
+++ src/Joke.java
@@ -0,0 +1,9 @@
+public class Joke {
+    public static void main(String[] args){
+
+        System.out.println("How many psychiatrists does it take to change a light bulb?");
+        System.out.println("Only one, but the light bulb has to WANT to change.");
+
+    }
+
+}
